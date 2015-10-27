b0e193fe5668252dfd4a86bd64f360f7b681eaaf
Brian McAllister <brian@brianmcallister.com>
2015-10-26 22:29:43 -0400

    Add boilerplate.

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// var path = require("path");
+// module.exports = {
+//   // entry: {
+//   //   app: ["./app/main.js"]
+//   // },
+//   output: {
+//     path: path.resolve(__dirname, "build"),
+//     publicPath: "/public/",
+//     filename: "bundle.js"
+//   }
+// };
