841e83447ded2b60603868b473c843b12b44cf62
Brian McAllister <brian@brianmcallister.com>
2015-10-28 19:18:15 -0400

    Add a clear button.

--- src/hello.jsx
+++ src/hello.jsx
@@ -13,13 +13,40 @@ export default class Hello extends React.Component {
     this.setState({name: event.currentTarget.value})
   }
 
+  clearInput() {
+    this.setState({name: null})
+  }
+
+  renderNameString() {
+    if (!this.state.name) {
+      return 'Hello. Try entering your name.'
+    }
+
+    return `Hello, ${this.state.name}`
+  }
+
   render() {
     return (
       <div>
-        <h1>helloooo {this.state.name}</h1>
+        <h1>{this.renderNameString()}</h1>
 
-        <input onKeyUp={this.handleKeyUp.bind(this)}></input>
+        <input value={this.state.name} placeholder="Your name here..." onChange={this.handleKeyUp.bind(this)}></input>
+
+        {getButton.call(this)}
       </div>
     )
   }
+}
+
+let getButton = function () {
+  // Return a button if there's a name set.
+  if (this.state.name) {
+    return <button onClick={this.clearInput.bind(this)}>{this.props.clearButtonLabel}</button>
+  }
+
+  return null
+}
+
+Hello.defaultProps = {
+  clearButtonLabel: 'Clear'
 }
