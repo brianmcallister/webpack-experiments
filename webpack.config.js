8092d01e53f8a432bdc4318f7cd2816faf0020e3
Brian McAllister <brian@brianmcallister.com>
2015-10-30 19:06:22 -0400

    Output compiled files based on name. (?)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
 // TODO: Look into https://www.npmjs.com/package/assets-webpack-plugin
 // to generate HTML that has paths for hashed filenames.
 
-// TODO: Check if it's possible to only use the extract-text-webpack-plugin
-// when building for production so that we can use HMR in dev, but generate
-// real css files when deploying.
-
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -17,7 +13,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     publicPath: 'http://localhost:8080/public',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     loaders: [
@@ -26,7 +22,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css')
+    new ExtractTextPlugin('[name].css')
   ],
   resolve: {
     extensions: ['', '.js', '.jsx']
